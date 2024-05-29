cf36d3d41335bf01064c814f410f72caa39ba862
Dnyaneshwar Magar <89983890+dnyaneshwarmagar@users.noreply.github.com>
2024-05-29 12:37:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -58,10 +58,11 @@ document.addEventListener('DOMContentLoaded', function() {
     const decodedCookie = document.cookie;
     const cookieArray = decodedCookie.split(';');
     for (let i = 0; i < cookieArray.length; i++) {
-      let cookie = cookieArray[i];
-      while (cookie.charAt(0) === ' ') {
-        cookie = cookie.substring(1);
-      }
+      let cookie = cookieArray[i].trim();
+    //   while (cookie.charAt(0) === ' ') {
+    //     cookie = cookie.substring(1);
+		  console.log("cookie",cookie)
+    //   }
       if (cookie.indexOf(cookieName) === 0) {
         return cookie.substring(cookieName.length, cookie.length);
       }
